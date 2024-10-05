1e4bb525a561ef2a63fb8da9d81bfcddc4407ff9
Younes <younes.zouhairi@student.ehb.be>
2024-10-05 19:00:36 +0200

    Update index page - Added images for skills - Modified CSS - Added keyframes for animations - Update index page with more information

--- PortofolioWebsite/wwwroot/js/site.js
+++ PortofolioWebsite/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Smooth Scroll
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
-// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    // Back to Top Button
+    const backToTopButton = document.getElementById('back-to-top');
+
+    // Check if the button element exists
+    if (backToTopButton) {
+        window.onscroll = function () {
+            // Show the button when the user scrolls 300px down
+            if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+                backToTopButton.style.display = 'block';
+            } else {
+                backToTopButton.style.display = 'none';
+            }
+        };
+
+        // Smooth scroll back to the top when the button is clicked
+        backToTopButton.addEventListener('click', function () {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
+
